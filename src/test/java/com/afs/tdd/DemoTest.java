4d3560035471c5cd1d8e9ffbc258e8aecec24fea
AUAL <alex.au@oocl.com>
2020-11-26 17:16:51 +0800

    [MarsRover][Alex Au] test: add test case for turn right from 0,0,E

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -18,4 +18,18 @@ class DemoTest {
         assertEquals(0, actual.getX());
         assertEquals(1, actual.getY());
     }
+
+    @Test
+    void should_mars_rover_return_0_0_E_when_turn_right() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, 'N');
+
+        //when
+        GeoInfo actual = marsRover.executeCommands("R");
+
+        //then
+        assertEquals('E', actual.getHeading());
+        assertEquals(0, actual.getX());
+        assertEquals(0, actual.getY());
+    }
 }
